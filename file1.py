2f41e08b44054718d6645d48dcf456c39f2cff25
Varin Nair <varinnair@gmail.com>
2018-01-29 10:47:25 -0800

    asdf

--- file1.py
+++ file1.py
@@ -1,3 +1,9 @@
-#Importing TensorFlow library
-import tensorflow as tf
-print("Hello, World!")
+from sklearn import tree
+from sklearn.datasets import load_iris
+
+features = [[140, 0], [130, 0], [150, 1], [170, 1]]
+labels = [0, 0, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print(clf.predict([[160, 0]]))
